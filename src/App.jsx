2eb84e2ced8d1a9b6c0ef50097a69497bca2212e
Roxaneryz <roxane5666@yahoo.gr>
2024-07-02 17:06:28 +0300

    starting the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,61 @@
-import { useEffect} from "react";
-import ContactList from "./components/ContactList/ContactList";
-import ContactForm from "./components/ContactForm/ContactForm";
-import SearchBox from "./components/SearchBox/SearchBox";
+// import { useEffect, lazy } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Route, Routes } from "react-router-dom";
+// import { selectIsRefreshing } from "../../redux/auth/selectors";
+// import { refreshUser } from "../../redux/auth/operations";
+// import Layout from "../Layout";
+// import RestrictedRoute from "../RestrictedRoute";
+// import PrivateRoute from "../PrivateRoute";
+// import { Toaster } from "react-hot-toast";
 
-import { useDispatch} from "react-redux";
-import { fetchContacts } from "./redux/contactsOps";
-// import { addContact, deleteContact, selectContacts } from "./redux/contactsSlice";
-// import { changeFilter, selectNameFilter } from "./redux/filtersSlice";
+// const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+// const RegistrationPage = lazy(() =>
+//   import("../../pages/RegistrationPage/RegistrationPage")
+// );
+// const LoginPage = lazy(() => import("../../pages/LoginPage/LoginPage"));
+// const ContactsPage = lazy(() =>
+//   import("../../pages/ContactsPage/ContactsPage")
+// );
 
+// const App = () => {
+//   const dispatch = useDispatch();
+//   const isRefreshing = useSelector(selectIsRefreshing);
 
+//   useEffect(() => {
+//     dispatch(refreshUser());
+//   }, [dispatch]);
 
+//   return isRefreshing ? (
+//     <b>Refreshing user...</b>
+//   ) : (
+//     <Layout>
+//       <Toaster position="top-center" reverseOrder={false} />
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route
+//           path="/register"
+//           element={
+//             <RestrictedRoute
+//               redirectTo="/contacts"
+//               component={<RegistrationPage />}
+//             />
+//           }
+//         />
+//         <Route
+//           path="/login"
+//           element={
+//             <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+//           }
+//         />
+//         <Route
+//           path="/contacts"
+//           element={
+//             <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+//           }
+//         />
+//       </Routes>
+//     </Layout>
+//   );
+// };
 
-const App = () => {
-
-  const dispatch = useDispatch();
-  
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
-
-  //  const contacts = useSelector(selectContacts);
-  // const filter = useSelector(selectNameFilter);
-  
-
-  // const [filter, setFilter] = useState("");
-
-  // const [contacts, setContacts] = useState(
-  //   () => JSON.parse(localStorage.getItem("saved-contacts")) ?? initialContacts
-  // );
-
-  // useEffect(() => {
-
-  //   const savedContacts = JSON.parse(localStorage.getItem("saved-contacts"));
-
-  //   if (savedContacts) {
-  //     savedContacts.forEach((conatct) => {
-  //       dispatch(addContact(conatct));
-  //    })
-  //  }
-  // }, [dispatch]);
-
-  // // console.log(contacts);
-  // const handleAddContact = (newContact) => {
-  //   dispatch(addContact(newContact));
-  // };
-
-  // const handleDeleteContact = (id) => {
-  //   dispatch(deleteContact(id));
-  // };
-
-  //  const handleChangeFilter = (value) => {
-  //    dispatch(changeFilter(value));
-  //  };
-  // const filteredContacts = () => {
-  //   return contacts.filter(
-  //     (contact) =>
-  //       contact.name.toLowerCase().includes(filter.toLowerCase()) ||
-  //       contact.number.includes(filter)
-  //   );
-  // };
-  // console.log(filteredContacts());
-  return (
-    <div>
-      <h1>PhoneBook</h1>
-      <ContactForm /> {/* /* addContact={handleAddContact} */}
-      <h3>Find your contact by name or number</h3>
-      <SearchBox /> {/* filter={filter} changeFilter={handleChangeFilter} */}
-      <ContactList /> {/* contacts={filteredContacts()}
-      deleteContact={handleDeleteContact} */}
-    </div>
-  );
-};
-
-export default App;
+// export default App;
