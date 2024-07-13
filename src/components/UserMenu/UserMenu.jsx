145d31517119343aed129cb955c53eddf0b3615e
Roxaneryz <roxane5666@yahoo.gr>
2024-07-13 18:59:10 +0300

    +

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -9,13 +9,19 @@ import { Typography } from "antd";
 
 const { Title } = Typography;
 
+// const TypographyWrapper = styled.div`
+//   color: white;
+// `;
+
 const UserMenu = () => {
   const dispatch = useDispatch();
     const user = useSelector(selectUser);
-  return (
-    <Flex gap="20px" color="#00000">
-      <Title className={css.userName} level={3}>Welcome, {user.name}</Title>
-      <button type="button" onClick={() => dispatch(logOut())}>Logout</button>
+  return ( 
+    <Flex gap="20px" className={css.userName}>
+      <Title level={3}>Welcome, {user.name}</Title>
+      <button type="button" onClick={() => dispatch(logOut())}>
+        Logout
+      </button>
     </Flex>
   );
 }
